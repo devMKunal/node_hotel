0e4137b87b641aeee44acf6865508a99994ccde9
Kunal Mahajan <mahajankunal550@gmail.com>
2026-06-10 00:51:46 +0530

    middlewares

--- models/person_model.js
+++ models/person_model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 /// Person Schema
 const personSchema = new mongoose.Schema({
@@ -31,8 +32,44 @@ const personSchema = new mongoose.Schema({
     salary: {
         type: Number,
         required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
     }
 })
+
+personSchema.pre('save', async function() {
+    const person = this;
+
+    // Has the password only if it has been modified (or is new)
+    if (!person.isModified('password')) return;
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password generation
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        person.password = hashedPassword;
+
+        return;
+    } catch (err) {
+        return ;
+    }
+})
+
+personSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        throw err;
+    }
+}
 
 /// Person Model
 
--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date()}] Requested to : ${req.originalUrl}`)
+    next();
+}
+
+app.use(logRequest);
+
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local', { session: false });
 
 
 app.get('/', (req, res) => {
@@ -13,7 +24,7 @@ app.get('/', (req, res) => {
 })
 
 const personRoutes = require('./routes/person_routes');
-app.use('/person', personRoutes)
+app.use('/person', localAuthMiddleware, personRoutes)
 
 const menuRoutes = require('./routes/menu_routes');
 app.use('/menu', menuRoutes)
